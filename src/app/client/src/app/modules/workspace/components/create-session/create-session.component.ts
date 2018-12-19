6d6cad6747d99d61558565b12bbe8b6e70081cda
aishwa8141 <a.aish1997@gmail.com>
2018-12-19 09:33:05 +0530

    reponsive

--- src/app/client/src/app/modules/workspace/components/create-session/create-session.component.ts
+++ src/app/client/src/app/modules/workspace/components/create-session/create-session.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import {CourseConsumptionService} from '../../../learn/services';
 
 @Component({
   selector: 'app-create-session',
   templateUrl: './create-session.component.html',
-  styleUrls: ['./create-session.component.css']
+  styleUrls: ['./create-session.component.css'],
+  // providers: [CourseConsumptionService]
 })
 export class CreateSessionComponent implements OnInit {
 
   existingSessionFlag: Boolean;
-
-  constructor(
+  course: any;
+  courseid: any;
+  coursechapters;
+  constructor(private courseConsumptionService: CourseConsumptionService,
     public dialogRef: MatDialogRef<CreateSessionComponent>,
     @Inject(MAT_DIALOG_DATA) private data) { }
 
@@ -22,7 +26,19 @@ export class CreateSessionComponent implements OnInit {
     if ('createSession' in this.data) {
       this.existingSessionFlag = false;
     }
-
+    console.log('meta data', this.data);
+    this.courseid = this.data.createSession.courseId;
+    this.getCourseUnits();
   }
 
+  getCourseUnits() {
+    this.courseConsumptionService.getCourseHierarchy(this.courseid)
+    .subscribe(
+      (response: any) => {
+        this.course = response;
+        this.coursechapters = this.course.children;
+        console.log('khdjwhsefdsfesjfhe', this.course);
+      }
+    );
+    }
 }
