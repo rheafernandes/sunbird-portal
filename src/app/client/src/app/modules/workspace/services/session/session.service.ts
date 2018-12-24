d4929f278e94fe82abb92e59720b2a62e91691a9
yravinderkumar33 <yravinderkumar33@gmail.com>
2018-12-24 15:09:04 +0530

    coaching session attendance done

--- src/app/client/src/app/modules/workspace/services/session/session.service.ts
+++ src/app/client/src/app/modules/workspace/services/session/session.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, OnInit } from '@angular/core';
 import { of, from } from 'rxjs';
 import { filter } from 'rxjs/operators';
+import { ToasterService } from '../../../shared/services/toaster/toaster.service';
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
   sessions = [];
-  constructor() {
+  constructor(private toasterService: ToasterService) {
     if (localStorage.getItem('sessions') !== null) {
       this.sessions = JSON.parse(localStorage.getItem('sessions'));
     }
@@ -53,6 +54,7 @@ export class SessionService {
       return false;
     });
     this.sessions[batchIndex] = session;
+    this.storeSessions();
   }
 
   deleteSession(session) {
@@ -68,7 +70,6 @@ export class SessionService {
     this.sessions.splice(batchIndex, 1);
   }
 
-
   publishSession(session) {
     const batchId = session.identifier;
     const courseId = session.courseId;
@@ -80,5 +81,37 @@ export class SessionService {
       return false;
     });
     this.sessions[batchIndex].sessionDetails.status = 'published';
+  }
+
+
+  joinSession(userId, session) {
+    console.log('sessionService : userID', userId);
+    console.log('sessionService : session', session);
+    const eligibilty = Object.keys(session.participant).includes(userId);
+    if (eligibilty) {
+
+      const batchId = session.identifier;
+      const courseId = session.courseId;
+      const courseUnitId = session.sessionDetails.courseUnit;
+      const batchIndex = this.sessions.findIndex((batch) => {
+        if (batch.identifier === batchId && batch.courseId === courseId && batch.sessionDetails.courseUnit === courseUnitId) {
+          return true;
+        }
+        return false;
+      });
+
+      const attendee = {
+        id: userId,
+        joinedAt: new Date()
+      };
+
+      const updatedSession = this.sessions[batchIndex];
+      updatedSession.sessionDetails.attendees.push(attendee);
+      updatedSession.sessionDetails.enrolledCount = updatedSession.sessionDetails.attendees.length;
+      this.updateSession(updatedSession);
+      console.log('attendance captured', updatedSession.sessionDetails);
+    } else {
+      this.toasterService.error('you are not eligible to join the session');
+    }
   }
 }
