8567bead74d34b4e650fadc167f2242331d338c3
yravinderkumar33 <yravinderkumar33@gmail.com>
2018-12-27 09:45:07 +0530

    view control added

--- src/app/client/src/app/modules/workspace/components/session-list/session-list.component.ts
+++ src/app/client/src/app/modules/workspace/components/session-list/session-list.component.ts
@@ -6,6 +6,7 @@ import { SessionService } from '../../services/session/session.service';
 import { SessionDetailsComponent } from '../session-details/session-details.component';
 import { AutofillMonitor } from '@angular/cdk/text-field';
 import { ToasterService } from '../../../shared/services/toaster/toaster.service';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-session-list',
   templateUrl: './session-list.component.html',
@@ -14,11 +15,22 @@ import { ToasterService } from '../../../shared/services/toaster/toaster.service
 export class SessionListComponent implements OnInit {
   constructor(private toasterService: ToasterService, public dialog: MatDialog, private sessionService: SessionService) { }
   sessionsList;
+  views = new FormControl();
+  currentView = [];
+  viewsList: string[] = ['startDate', 'endDate', 'time', 'status', 'attendance'];
 
   ngOnInit() {
+    this.views.valueChanges.subscribe((data) => {
+      localStorage['VIEW_CONTROL'] = JSON.stringify(data);
+      this.currentView = data;
+    });
     this.sessionService.getSessions().subscribe((sessions) => {
       this.sessionsList = sessions;
     });
+
+    if (localStorage.getItem('VIEW_CONTROL') !== null) {
+      this.currentView = JSON.parse(localStorage.getItem('VIEW_CONTROL'));
+    }
   }
 
   openDialog(session): void {
@@ -60,5 +72,13 @@ export class SessionListComponent implements OnInit {
   showValues(value) {
     console.log('form data', value);
   }
+
+  view(attr) {
+    if (this.currentView.length === 0) {
+      return true;
+    }
+    return this.currentView.includes(attr);
+  }
+
 }
 
